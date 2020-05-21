a01fabc65adc2913fb5da8957910a429426cb6e9
Leopoldo Varela <pistolilla58@hotmail.com>
2020-05-21 18:27:18 -0500

    genres + results ready

--- movielens/index.js
+++ movielens/index.js
@@ -1,11 +1,13 @@
+const API_URL = "https://pistolilla.pythonanywhere.com";
+
 var routes = [
     {
-        path: '/movielens/', component: {
-            template: ""
+        path: '/', component: {
+            template: "<div></div>"
         }
     },
     {
-        path: '/movielens/about', component: {
+        path: '/about', component: {
             template: `<div id="aboutbox" class="alert alert-dismissible alert-primary m-2">
             <button type="button" class="close" data-dismiss="alert">&times;</button>
             AI Movies Recommender is fueled with the MovieLens Dataset from <a href="https://grouplens.org/" target="_blank">Group Lens</a>. Source code and more details <a href="https://github.com/pistolilla/movielens" target="_blank">here</a>.
@@ -15,12 +17,68 @@ var routes = [
 ];
 
 var router = new VueRouter({
-  routes: routes,
-  mode: 'history',
-  base: '/'
+    routes: routes,
+    mode: 'history',
+    base: '/movielens'
 });
 
 var app = new Vue({
 	el: '#app',
-  	router: router
+    router: router,
+    data: {
+        // gifs
+        showRecommendationsGif: false,
+        showResultsGif: false,
+        showInfoGif: true,
+        showGenresGif: false,
+        // controls
+        search: "",
+        // collections
+        genres: [],
+        results: [],
+    },
+    methods: {
+        // common
+        prepend: function(adp, str) {
+            return String(adp) + String(str);
+        },
+        sayHi: function(str) {
+            console.log("hola " + str);
+        },
+        // api calls
+        getMovies: function() {
+            this.showResultsGif = true;
+            axios({
+                method: "POST",
+                url: `${API_URL}/movielens/api/movies`,
+                data: {
+                    genres: this.genres.filter(obj => obj.checked).map(obj => obj.value),
+                    search: this.search,
+                }
+            })
+            .then(function(response) {
+                this.app.results = response.data;
+            })
+            .catch(error => console.log(error))
+            .finally(() => { this.showResultsGif = false });
+        },
+    },
+    // listeners
+    watch: {
+        genres: {
+            handler: function(val, oldval) {
+                this.getMovies();
+            },
+            deep: true
+        }
+    },
+    created() {
+        axios.get(`${API_URL}/movielens/api/genres`)
+        .then(function(response) {
+            this.app.genres = response.data.map(function(obj) {
+                return { value: obj, checked: true };
+            });
+        })
+        .catch(error => console.log(error));
+    },
 });
