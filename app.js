e989230e2dd9cf1c9ff2be748a50291501f95390
Leopoldo Varela <pistolilla58@hotmail.com>
2020-05-31 14:51:38 -0500

    app.js scalability

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
+const PROJECT_DIR = "/movielens";
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use('/movielens/', express.static(path.join(__dirname + '/movielens')));
-app.get('/movielens/',function(req, res){
-  res.sendFile(path.join(__dirname + '/movielens/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
+app.use(PROJECT_DIR, express.static(path.join(__dirname + PROJECT_DIR)));
 app.listen(process.env.port || port);
+console.log(`Running at http://localhost:${port}${PROJECT_DIR}/index.html`);
 
 // $ npm init --y
 // $ npm install --save express
 // $ node app.js
-console.log(`Running at http://localhost:${port}/movielens`);
